49efb218f9d6cd87c27a4ec67c989db889118f27
Rickey Chiu <chiurickey@gmail.com>
2025-10-04 17:04:10 -0400

    um

--- budgeteer/src/App.js
+++ budgeteer/src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="p-10 bg-gray-50 min-h-screen">
-      <h1 className="text-3xl font-bold mb-8">Budgeteer Dashboard 💰</h1>
+      <h1 className="text-3xl font-bold mb-8">Dashboard</h1>
 
       {/* Charts grid */}
       <div className="grid grid-cols-2 gap-8 mb-8">
